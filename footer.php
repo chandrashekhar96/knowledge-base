7c6c8c1ccd4364dcfec1d8ff69104e1128fd2d0d
V CHANDRA SHEKHAR <csvicky51@gmail.com>
2019-03-31 14:14:18 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+						<br class="clear" />
+					</div>
+					<br class="clear" />
+				</div>
+			</div>
+			<div id="copyright">
+				&copy; Knowledge Base 
+			</div>
+		</div>
+    </body>
+</html>
